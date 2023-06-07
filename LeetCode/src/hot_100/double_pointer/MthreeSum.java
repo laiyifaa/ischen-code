745b776f4e87dd6988fa850ea56dac62698685c2
laiyifaa <512879089@qq.com>
2023-06-07 13:57:17 +0800

    leetcode 283. 移动零 双指针 一个遍历数组的循环变量i 一个是记录0的个数的变量k，通过 nums[i] = nums[i+k] 使得非0元素前移。 leetcode 15. 三数之和

---
+++ LeetCode/src/hot_100/double_pointer/MthreeSum.java
@@ -0,0 +1,10 @@
+package hot_100.double_pointer;
+
+import java.util.List;
+
+public class MthreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        return null;
+    }
+}
