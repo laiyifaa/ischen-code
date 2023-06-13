360bec4a589efa2e3214cf81314f534e2bf1975a
laiyifaa <512879089@qq.com>
2023-06-13 19:16:20 +0800

    leetcode  73. 矩阵置零 思路：要使用原地算法 首先确认首行首列是否有0元素并用2个变量标记 也就是说将矩阵中非首行首列元素标记到矩阵的首行首列 最后遍历首行首列 对应的刷0 在查看俩个标记 刷0

--- LeetCode/src/hot_100/martix/MsetZeroes.java
+++ LeetCode/src/hot_100/martix/MsetZeroes.java
@@ -4,13 +4,13 @@ public class MsetZeroes {
 
     private void setRow(int[][] matrix ,int index){
         int a[] = matrix[index];
-        for(int i = 0;i<a.length;i++){
+        for(int i = 1;i<a.length;i++){
             a[i] = 0;
         }
     }
     private void setCol(int[][] matrix,int index){
-        for(int i = 0;i<matrix.length;i++){
-            matrix[index][i] = 0;
+        for(int i = 1;i<matrix.length;i++){
+            matrix[i][index] = 0;
         }
     }
     public void setZeroes(int[][] matrix) {
@@ -18,13 +18,55 @@ public class MsetZeroes {
             return;
         int m = matrix.length;
         int n = matrix[0].length;
-        for(int i = 0;i < m;i ++){
-
-
-            for(int j = 0;j<n;j++){
-
+        boolean firstRow = false;
+        boolean firstCol = false;
+        for(int i = 0;i < n;i ++){
+            if(matrix[0][i] == 0){
+                firstRow = true;
+                break;
             }
-
         }
+        for(int i = 0;i < m;i++){
+            if(matrix[i][0] == 0){
+                firstCol = true;
+                break;
+            }
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0;j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i = 1;i < n;i++){
+            if(matrix[0][i] == 0)
+                setCol(matrix,i);
+        }
+        for(int i = 1;i< m;i++){
+            if(matrix[i][0] == 0)
+                setRow(matrix,i);
+        }
+        if(firstRow)
+            for(int i = 0;i<n;i++){
+                matrix[0][i] = 0;
+            }
+        if(firstCol)
+            for(int i = 0;i<m;i++){
+                matrix[i][0] = 0;
+            }
+    }
+    public static void main(String[] args) {
+        MsetZeroes m = new MsetZeroes();
+        int[][] a = new int[][]
+                {
+                     /*   {0,1,2,0},
+                        {3,4,5,2},
+                        {1,3,1,5}*/
+                        {1,0}
+                    };
+        m.setZeroes(a);
     }
 }
