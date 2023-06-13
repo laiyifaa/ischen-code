ede887aafdba01cb7b61ce4953b35e4b6d34a45c
laiyifaa <512879089@qq.com>
2023-06-13 20:33:13 +0800

    leetcode  141. 环形链表  快慢指针 注意判空

---
+++ LeetCode/src/hot_100/list/hasCycle.java
@@ -0,0 +1,31 @@
+package hot_100.list;
+
+public class hasCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if(null == head || null == head.next )
+            return false;
+        ListNode quick = head;
+        ListNode slow = head;
+        while (true){
+            if(slow == null || quick == null)
+                return false;
+
+            quick = quick.next;
+            if(null != quick)
+                quick = quick.next;
+            else return false;
+            slow = slow.next;
+            if(quick == slow)
+                return true;
+        }
+    }
+    public static void main(String[] args){  
+        hasCycle q = new hasCycle();
+        ListNode x = new ListNode(1);
+        x.next = new ListNode(2);
+        x.next.next = null;
+        System.out.println(q.hasCycle(x));
+    }
+
+}
