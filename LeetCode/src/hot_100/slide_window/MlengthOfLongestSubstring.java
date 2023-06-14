f9385619a548183e9a73ea9d1fac16e8ffe5fe11
laiyifaa <512879089@qq.com>
2023-06-14 15:54:22 +0800

    leetcode  3. 无重复字符的最长子串 经典滑动窗口，不用map 会好点，用byte数组记录自负出现次数，多于1则开始移动窗口

---
+++ LeetCode/src/hot_100/slide_window/MlengthOfLongestSubstring.java
@@ -0,0 +1,39 @@
+package hot_100.slide_window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MlengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        if(null == s || 0 == s.length()){
+            return 0;
+        }
+        int ans = 0;
+        int l = 0;
+        int r = 0;
+        byte[] used = new byte[128];
+        int temp = 0;
+        while (l<=r  ){
+            if(r == s.length()){
+                break;
+            }
+            if(used[s.charAt(r) ] == 0){
+                used[s.charAt(r)]++;
+                temp++;
+                r++;
+            }else{
+                used[s.charAt(l) ]--;
+                l++;
+                temp--;
+            }
+            if(temp > ans)
+                ans = temp;
+        }
+        return ans;
+    }
+    public static void main(String[] args){  
+        MlengthOfLongestSubstring q = new MlengthOfLongestSubstring();
+        System.out.println(  q.lengthOfLongestSubstring(" "));
+    }
+}
