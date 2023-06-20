5bc99450f9caf9e18c30ba7beeecbbf35896a5dc
laiyifaa <512879089@qq.com>
2023-06-20 12:40:57 +0800

    leetcode 24. 两两交换链表中的节点 巧妙递归交换  先交换后面的节点。再将前驱节点的next连接后面，将后继结点的next 连接当前 莽夫思路可能就是开数组 交换再存回链表

---
+++ LeetCode/src/hot_100/list/MIsChen_swapPairs.java
@@ -0,0 +1,13 @@
+package hot_100.list;
+
+public class MIsChen_swapPairs {
+    
+    public ListNode swapPairs(ListNode head) {
+        if(null == head || null == head.next)
+            return head;
+        ListNode next = head.next;
+        head.next =  swapPairs(next.next);
+        next.next =head;
+        return next;
+    }
+}
