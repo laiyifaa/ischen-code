612ed8888bad3fdfd91f005240f4b06c38ad64cd
laiyifaa <512879089@qq.com>
2023-06-24 16:47:44 +0800

    leetcode 19. 删除链表的倒数第 N 个结点 递归解决，加入变量cnt记录，记录到-1 开始删除。 也要注意 1->2->3->null n = 3 时 cnt = 0时就是要删除头结点

---
+++ LeetCode/src/hot_100/list/MremoveNthFromEnd.java
@@ -0,0 +1,35 @@
+package hot_100.list;
+
+public class MremoveNthFromEnd {
+    private int cnt = 0;
+    private void go(ListNode head){
+        if(null != head.next)
+            go(head.next);
+        cnt--;
+        if(cnt == -1)
+            head.next = head.next.next;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(null == head ||0 == n)
+            return head;
+        if(null == head.next && 1 ==n){
+            return null;
+        }
+        cnt = n;
+        go(head);
+        if(0 == cnt)
+            return head.next;
+        return head;
+    }
+
+    public static void main(String[] args){  
+        ListNode x = new ListNode(1);
+        x.next = new ListNode(2);
+/*        x.next = new ListNode(2);
+        x.next.next = new ListNode(3);
+        x.next.next.next = new ListNode(4);
+        x.next.next.next.next = new ListNode(5);*/
+        MremoveNthFromEnd q = new MremoveNthFromEnd();
+        System.out.println(q.removeNthFromEnd(x,2));
+    }
+}
