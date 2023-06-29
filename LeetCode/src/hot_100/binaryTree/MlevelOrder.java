641e6641e6201c92ba990cc4f372092f6f7a2eb5
laiyifaa <512879089@qq.com>
2023-06-29 21:49:14 +0800

    leetcode 108. 将有序数组转换为二叉搜索树 思路：利用数组有序这一特点和二叉搜搜有序这一特点，即头结点为中点节点，那么以此递归（分治）建树即可

--- LeetCode/src/hot_100/binaryTree/MlevelOrder.java
+++ LeetCode/src/hot_100/binaryTree/MlevelOrder.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class MlevelOrder {
+    //102. 二叉树的层序遍历
     public List<List<Integer>> levelOrder(TreeNode root) {
         if(null == root)
             return new LinkedList<>();
