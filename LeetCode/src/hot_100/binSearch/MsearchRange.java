e0997dea96ed433747809d547dd34bef4edc4b3f
laiyifaa <512879089@qq.com>
2023-07-07 09:34:27 +0800

    leetcode 34. 在排序数组中查找元素的第一个和最后一个位置 我的思路：二分查找x=target-1 和 y=target+1的下标 那就分为找得到 和找不到 ，基于x 则++x 看看是否匹配target 同理--y看看是否匹配target 注意越界。 当然还有完美的思路

---
+++ LeetCode/src/hot_100/binSearch/MsearchRange.java
@@ -0,0 +1,48 @@
+package hot_100.binSearch;
+
+public class MsearchRange {
+    private int binSearch(int [] nums,int target){
+        int l = 0;
+        int r = nums.length - 1;
+        while (l<=r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid ] == target)
+                return mid;
+            else if(nums[mid] > target){
+                r = mid - 1;
+            }else {
+                l = mid + 1;
+            }
+        }
+        if(l<0)
+            l = 0;
+        if(l>=nums.length)
+            l = nums.length-1;
+        return l;
+    }
+    //34. 在排序数组中查找元素的第一个和最后一个位置
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[]{-1,-1};
+        if(null == nums || 0 == nums.length)
+            return ans;
+        int index = binSearch(nums,target - 1);
+        while (index<nums.length-1&&nums[index]<target){
+            ++index;
+        }
+        if( nums[index] != target)
+            index = -1;
+        ans[0] = index;
+        index = binSearch(nums,target + 1);
+        while (index>0&&nums[index]>target)
+            --index;
+        if( nums[index] != target)
+            index = -1;
+        ans[1] = index;
+        return ans;
+    }
+    public static void main(String[] args){  
+        int[] a = new int[]{6,8,8,8,8,8,8,8,8,11};
+        MsearchRange q = new MsearchRange();
+        q.searchRange(new int[]{-99999},0);
+    }
+}
