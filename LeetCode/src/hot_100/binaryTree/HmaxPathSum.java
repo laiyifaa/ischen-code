f4249861252a4fc3fbe3473a752c1604b2303a89
laiyifaa <512879089@qq.com>
2023-07-13 21:20:35 +0800

    leetcode 124. 二叉树中的最大路径和 test

--- LeetCode/src/hot_100/binaryTree/HmaxPathSum.java
+++ LeetCode/src/hot_100/binaryTree/HmaxPathSum.java
@@ -3,10 +3,7 @@ package hot_100.binaryTree;
 
 public class HmaxPathSum {
     int ans = Integer.MIN_VALUE;
-<<<<<<< Updated upstream
-=======
     //124. 二叉树中的最大路径和
->>>>>>> Stashed changes
     public int maxPathSum(TreeNode root) {
         if(null == root)
             return 0;
