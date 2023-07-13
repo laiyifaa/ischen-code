386a3b84f8e9493d58dd9db46f4f79d114dce557
laiyifaa <512879089@qq.com>
2023-07-13 19:45:11 +0800

    leetcode 加入根据数组创建树的函数，方便以后测试

--- LeetCode/src/hot_100/binaryTree/MIsChen_pathSum.java
+++ LeetCode/src/hot_100/binaryTree/MIsChen_pathSum.java
@@ -5,9 +5,20 @@ public class MIsChen_pathSum {
     public int pathSum(TreeNode root, int targetSum) {
         if(null == root)
             return 0;
+       /* return dfs(root,targetSum) + pathSum(root.left,targetSum) + pathSum(root.right,targetSum);*/
         return dfs(root,targetSum) + pathSum(root.left,targetSum) + pathSum(root.right,targetSum);
     }
-    private int dfs(TreeNode root,long sum ){
+    private int dfs(TreeNode root,long sum){
+        if(null == root)
+            return 0;
+        int ans = 0;
+        if(sum   == root.val)
+            ++ans;
+        ans += dfs(root.left,sum - root.val);
+        ans += dfs(root.right,sum - root.val);
+        return ans;
+    }
+    /*private int dfs(TreeNode root,long sum ){
         if(null ==  root)
             return 0;
         int ans = 0;
@@ -15,5 +26,11 @@ public class MIsChen_pathSum {
             ++ans;
          ans  = ans + dfs(root.left,sum - root.val) + dfs(root.right,sum - root.val);
          return ans;
+    }*/
+    public static void main(String[] args){
+        TreeNode root = TreeNode.create(
+                new Integer[] {10,5,-3,3,2,null,11,3,-2,null,1});
+        MIsChen_pathSum q = new MIsChen_pathSum();
+       System.out.println( q.pathSum(root,8));
     }
 }
