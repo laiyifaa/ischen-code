11d8ca6382a4b09362bd8accecc1e648cde74cff
laiyifaa <512879089@qq.com>
2023-07-22 17:04:27 +0800

    leetcode153. 寻找旋转排序数组中的最小值 题意:一个数组 由1个有序数组旋转可得。那么这个数组即是2个有序数组，且这俩个有序数组的元素不重复，并且A数组里面元素要么大于B数组元素，要么小于。查询最小值 ologn级别 思路:有序就肯定支持二分，要么分治二分，要么整体二分，只要尾元素大于中间元素 那么就缩小区间，否则就减少r

--- LeetCode/src/hot_100/binSearch/MfindMin.java
+++ LeetCode/src/hot_100/binSearch/MfindMin.java
@@ -2,8 +2,26 @@ package hot_100.binSearch;
 
 public class MfindMin {
     public int findMin(int[] nums) {
-        return 0;
+        if(null == nums || 0 == nums.length)
+            return 0;
+        int l = 0;
+        int r = nums.length - 1;
+        if(1 == nums.length || (nums[l]<nums[r]))
+            return nums[0];
+        while(l<=r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] > nums[r]){
+                l = mid + 1;
+            }else {
+                --r;
+            }
+        }
+        return nums[l];
     }
     public static void main(String[] args){
+        MfindMin q = new MfindMin();
+        System.out.println(q.findMin(new int[]{3,4,5,1,2}));
+        System.out.println(q.findMin(new int[]{4,5,6,7,0,1,2}));
+        System.out.println(q.findMin(new int[]{11,13,15,17}));
     }
 }
