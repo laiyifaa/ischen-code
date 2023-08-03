5d91caa5473b30bb4c25b70a3df38ade9984808a
laiyifaa <512879089@qq.com>
2023-08-03 16:11:07 +0800

    leetcode153. 寻找旋转排序数组中的最小值 题意:一个数组 由1个有序数组旋转可得。那么这个数组即是2个有序数组，且这俩个有序数组的元素不重复，并且A数组里面元素要么大于B数组元素，要么小于。查询最小值 ologn级别 思路:有序就肯定支持二分，要么分治二分，要么整体二分，只要尾元素大于中间元素 那么就缩小区间，否则就减少r

--- LeetCode/src/hot_100/recall/Mpermute.java
+++ LeetCode/src/hot_100/recall/Mpermute.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mpermute {
-
+    //46 全排列
     List<List<Integer>> ansList ;
     public List<List<Integer>> permute(int[] nums) {
         ansList = new ArrayList<>(getMult(nums.length));
