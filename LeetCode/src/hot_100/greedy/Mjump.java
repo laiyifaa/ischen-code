34207c9bfb8a6c291a98e0f276a2e90937bd9674
laiyifaa <512879089@qq.com>
2023-08-29 15:41:06 +0800

    leetcode  45. 跳跃游戏 II 思路：首先 num.len == 0 || == 1 不用跳就满足了 其次 开循环遍历，如果当前的i 满足 num[i] + i > num.len - 1 即循环结束 否则 开始寻找 自己能跳的下一个j 中最大的那个 标记为maxi ，i = maxi 直至循环结束 打印cnt

---
+++ LeetCode/src/hot_100/greedy/Mjump.java
@@ -0,0 +1,35 @@
+package hot_100.greedy;
+
+public class Mjump {
+
+    //45. 跳跃游戏 II
+    public int jump(int[] nums) {
+        if(null == nums || nums.length == 0 || nums.length == 0)
+            return 0;
+        int cnt = 1;
+        int i = 0;
+        while (true){
+            int maxi = i;
+            if(nums[i] == 0)
+                return cnt;
+            if(nums[i] + i >= nums.length - 1)
+                break;
+            for(int j = i + 1 ; j <= i + nums[i] && j < nums.length; ++j){
+                if(nums[j] + j  > nums[maxi] + maxi){
+                    maxi = j;
+                }
+            }
+            ++cnt;
+            if(nums[maxi] + maxi >= nums.length - 1)
+                return cnt;
+            i = maxi;
+        }
+        return cnt;
+    }
+    public static void main(String[] args){
+        Mjump q = new Mjump();
+        System.out.println(q.jump(new int[]{
+       1,2
+        }));
+    }
+}
