902bb04b4aa93295cd0efd42166ac164bf31391c
laiyifaa <512879089@qq.com>
2023-08-31 15:56:10 +0800

    leetcode 39. 组合总和 思路：一看题目就知道得深搜的，而且又是求目标和，是得开个标技术组used[] 再深搜前排序一下，方便剪枝 开始设计深搜函数，递归终止条件很简单 sum==target 就行 加入index 变量 是防止重复 比如 2 2 3 和 2 3 2 以及 3 2 2

---
+++ LeetCode/src/hot_100/recall/MIsChen_combinationSum.java
@@ -0,0 +1,40 @@
+package hot_100.recall;
+
+import java.util.*;
+
+public class MIsChen_combinationSum {
+
+    List<List<Integer>> ans ;
+    int[] used;
+    private void dfs (int[] candidates, int target,int sum,int index){
+        if(sum == target){
+            List<Integer> list = new LinkedList<>();
+            for(int i = 0;i < candidates.length; ++i){
+                for(int j = 0; j < used[i];++j){
+                    list.add(candidates[i]);
+                }
+            }
+            ans.add(list);
+            return;
+        }
+        for(int i = index ;i < candidates.length;++i){
+            if(sum + candidates[i] > target){
+                break;
+            }
+            ++used[i];
+            dfs(candidates,target,sum + candidates[i],i);
+            --used[i];
+        }
+    }
+    //39. 组合总和
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        ans = new LinkedList<>();
+        if(null == candidates ||  candidates.length == 0)
+            return ans;
+        used = new int[candidates.length];
+        Arrays.sort(candidates);
+        dfs(candidates,target,0,0);
+        return ans;
+    }
+}
+
