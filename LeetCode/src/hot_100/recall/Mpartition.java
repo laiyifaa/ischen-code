e51a4fed0aa196262c6541bc7d729c12de62101e
laiyifaa <512879089@qq.com>
2023-09-01 15:59:56 +0800

    leetcode 131. 分割回文串 思路1：通过暴力的方法 枚举出所有的子串集合 然后比对是否满足全部回文 效率很低（开一个标记数组 用1切割字符串） getList 思路2：不通过暴力的方法，先判断第一个子串是回文才允许往下搜，减少计算

--- LeetCode/src/hot_100/recall/Mpartition.java
+++ LeetCode/src/hot_100/recall/Mpartition.java
@@ -4,24 +4,21 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Mpartition {
-    List<List<String>> ans;
-    int[] used;
-    int len = 0;
+    List<List<String>> ans ;
+    List<String> path = new LinkedList<>();
     private void dfs(int step,String s){
-        if(step == len - 1){
-            List<String> list = getList(s);
-            for(String x : list){
-                if (!check(x)){
-                    return;
-                }
-            }
-            ans.add(list);
+        if(step == s.length()){
+            ans.add(new LinkedList<>(path));
             return;
         }
-        used[step] = 1;
-        dfs(step + 1,s);
-        used[step] = 0;
-        dfs(step + 1,s);
+        for (int i = step ; i < s.length(); ++i){
+            String temp = s.substring(step, i + 1);
+            if(check(temp)){
+                path.add(temp);
+                dfs(i + 1,s);
+                path.remove(path.size() - 1);
+            }
+        }
     }
     private boolean check(String  x){
         if(x.length() == 1 )
@@ -36,33 +33,17 @@ public class Mpartition {
         }
         return true;
     }
-    private List<String>  getList(String s){
-        List<String> list = new LinkedList<>();
-        StringBuilder temp = new StringBuilder();
-        for(int i = 0; i < used.length - 1; ++i){
-            temp.append(s.charAt(i));
-            if(used[i] == 1){
-                list.add(new String(temp));
-                temp = new StringBuilder();
-            }
-        }
-        temp.append(s.charAt(used.length - 1));
-        list.add(new String(temp));
-        return list;
-    }
     //131. 分割回文串
     public List<List<String>> partition(String s) {
         ans = new LinkedList<>();
         if(null == s || s.length() == 0)
             return ans;
-        len = s.length();
-        used = new int[len];
         dfs(0,s);
         return ans;
     }
     public static void main(String[] args){
         Mpartition q = new Mpartition();
-        List<List<String>> list = q.partition("a");
+        List<List<String>> list = q.partition("aab");
         for(List<String> l : list){
             for(String  x  : l){
                 System.out.print(x);
