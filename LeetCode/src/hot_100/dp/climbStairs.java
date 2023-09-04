fb8237aa12dd32abddc105487819645f36edbf53
laiyifaa <512879089@qq.com>
2023-09-04 11:26:27 +0800

    leetcode 70. 爬楼梯          思路1：简单dp          思路2：矩阵快速幂 2的5次 101

--- LeetCode/src/hot_100/dp/climbStairs.java
+++ LeetCode/src/hot_100/dp/climbStairs.java
@@ -2,11 +2,31 @@ package hot_100.dp;
 
 public class climbStairs {
         // f(n+1) = f(n) + f(n-1)
-/*    private int fun_matrix(int n){
+   private int fun_matrix(int n){
         int[][] q = {{1, 1}, {1, 0}};
         int[][] res = pow(q, n);
         return res[0][0];
-    }*/
+    }
+    private int[][] pow(int[][]q,int n){
+        int[][] e = {{1, 0}, {0, 1}}; //单位矩阵
+       while(n > 0){
+           if((n&1) == 1){
+               e = mult(e,q);
+           }
+           n >>= 1;
+           q = mult(q,q);
+       }
+       return e;
+    }
+    private int[][] mult(int [][]a,int [][]b){
+       int [][] c = new int[2][2];
+       for(int i = 0;i < 2;++i){
+           for(int j = 0;j < 2; ++j){
+               c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+           }
+       }
+       return c;
+    }
     private int fun_dp(int n){
         if(n == 1 || n == 2)
             return n;
@@ -21,6 +41,6 @@ public class climbStairs {
     }
     //70. 爬楼梯
     public int climbStairs(int n) {
-        return fun_dp(n);
+        return fun_matrix(n);
     }
 }
