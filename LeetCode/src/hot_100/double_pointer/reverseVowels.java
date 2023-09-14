3deb69a0150538d0139fe6472546e8eef2632925
laiyifaa <512879089@qq.com>
2023-09-14 15:19:29 +0800

    leetcode 151 345 2道字符串 简单题

---
+++ LeetCode/src/hot_100/double_pointer/reverseVowels.java
@@ -0,0 +1,26 @@
+package hot_100.double_pointer;
+
+import java.util.LinkedList;
+
+public class reverseVowels {
+    //345. 反转字符串中的元音字母
+    public String reverseVowels(String s) {
+        if(null == s || s.length() == 0)
+            return s;
+        char[] chars = s.toCharArray();
+        LinkedList<Character> temp = new LinkedList<>();
+        for(int i = 0;i<chars.length;++i){
+            if(chars[i] == 'a' || chars[i] == 'e' || chars[i] == 'i' || chars[i] == 'o' || chars[i] == 'u'
+            ||chars[i] == 'A' || chars[i] == 'E' || chars[i] == 'I' || chars[i] == 'O' || chars[i] == 'U'){
+                temp.addFirst(chars[i]);
+            }
+        }
+        for(int i = 0;i < chars.length;++i){
+            if(chars[i] == 'a' || chars[i] == 'e' || chars[i] == 'i' || chars[i] == 'o' || chars[i] == 'u'
+                    ||chars[i] == 'A' || chars[i] == 'E' || chars[i] == 'I' || chars[i] == 'O' || chars[i] == 'U'){
+                chars[i] = temp.pollFirst();
+            }
+        }
+        return new String(chars);
+    }
+}
