85b1eeb1078f5d2f546ef337cc35a1cb902bcb97
laiyifaa <512879089@qq.com>
2023-12-06 11:04:00 +0800

    leetcode面试题 05.04. 下一个数 不需要位运算，直接暴力模拟即可，因为1的个数相同的话，差距不大。 +1 -1 然后不断匹配校验即可

---
+++ LeetCode/src/leetcode/binaryTree/MfindClosedNumbers.java
@@ -0,0 +1,44 @@
+package leetcode.binaryTree;
+
+public class MfindClosedNumbers {
+    //面试题 05.04. 下一个数
+    public int[] findClosedNumbers(int num) {
+        int minx = -1;
+        int maxy = -1;
+        int cnt = getCnt(num);
+        int top = (num << 1 ) < 0 ? Integer.MAX_VALUE:num<<1;
+        if(num != Integer.MAX_VALUE){
+            for(int i = num + 1;i <= top;++i){
+                if(getCnt(i) == cnt){
+                    maxy = i;
+                    break;
+                }
+            }
+        }
+        if(num != Integer.MAX_VALUE){
+            for(int i = num - 1;i >= Math.max(1,num >>1);--i){
+                if(getCnt(i) == cnt){
+                    minx = i;
+                    break;
+                }
+            }
+        }
+        return new int[]{maxy,minx};
+    }
+
+    private int getCnt(int num) {
+        int cnt = 0;
+        while (num > 0){
+            if((num & 1) == 1)
+                ++cnt;
+            num >>>=1;
+        }
+        return cnt;
+    }
+    public static void main(String[] args){
+        MfindClosedNumbers q = new MfindClosedNumbers();
+        int[] closedNumbers = q.findClosedNumbers(2147483647);
+        System.out.println(closedNumbers[0]);
+        System.out.println(closedNumbers[1]);
+    }
+}
