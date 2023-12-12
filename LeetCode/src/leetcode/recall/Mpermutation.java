9ae7feb197f3b151ee92bcda7b4c8e0a052f74bb
laiyifaa <512879089@qq.com>
2023-12-12 10:57:32 +0800

    leetcode 面试题 面试题 08.08. 有重复字符串的排列组合 回溯 生成排列

--- LeetCode/src/leetcode/recall/Mpermutation.java
+++ LeetCode/src/leetcode/recall/Mpermutation.java
@@ -1,7 +1,9 @@
 package leetcode.recall;
 
+import java.util.HashSet;
+
 public class  Mpermutation {
-    //面试题 08.07. 无重复字符串的排列组合
+   /* //面试题 08.07. 无重复字符串的排列组合
     String[] ans;
     boolean[] used;
     int cnt ;
@@ -36,10 +38,39 @@ public class  Mpermutation {
             x*=i;
         }
         return x;
+    }*/
+    //面试题 08.08. 有重复字符串的排列组合
+    String[] ans;
+    boolean[] used;
+    HashSet<String> set;
+    public String[] permutation(String S) {
+        used = new boolean[S.length()];
+        set = new HashSet<>(S.length() * S.length());
+        dfs(new StringBuilder(),S);
+        return set.toArray(new String[set.size()]);
+    }
+
+    private void dfs(StringBuilder temp,String S) {
+        if(temp.length()  == S.length()){
+            String s = temp.toString();
+            if(!set.contains(s))
+                set.add(s);
+            return;
+        }
+
+        for(int i = 0 ; i < S.length() ;++i){
+            if(used[i] == true)
+                continue;
+            StringBuilder y = new StringBuilder(temp.append(S.charAt(i)));
+            used[i] = true;
+            dfs(y,S);
+            used[i] = false;
+            temp.deleteCharAt(temp.length() - 1);
+        }
     }
     public static void main(String[] args){
         Mpermutation q = new Mpermutation();
-        String[] qwes = q.permutation("qwe");
+        String[] qwes = q.permutation("qqe");
         for(String x : qwes){
             System.out.println(x);
         }
