7135c24f51b4b92f6785d54368639100a4b0acd7
laiyifaa <512879089@qq.com>
2023-12-15 12:43:47 +0800

    leetcode 面试题 10.03. 搜索旋转数组 有序二分 找最小索引

--- LeetCode/src/leetcode/binSearch/MIsChen_search.java
+++ LeetCode/src/leetcode/binSearch/MIsChen_search.java
@@ -2,24 +2,54 @@ package leetcode.binSearch;
 
 public class MIsChen_search {
     //33. 搜索旋转排序数组
+/*    public int search(int[] nums, int target) {
+        return divide(0,nums.length -1,nums,target);
+    }*/
+    //面试题 10.03. 搜索旋转数组
+
     public int search(int[] nums, int target) {
         return divide(0,nums.length -1,nums,target);
     }
     private int divide(int l,int r,int[] nums,int target){
         if(l>r)
             return -1;
-        if(nums[l]<nums[r])
+        if(nums[l] < nums[r])
             return binSearch(l,r,nums,target);
-        int mid = l + (r - l)/2;
-        if(nums[mid] == target)
-            return mid;
-        return Math.max(divide(l,mid-1,nums,target),divide(mid+1,r,nums,target));
+
+        int mid = getMid(l,r,nums,target);
+        if(nums[mid] == target){
+            int lres = divide(l, mid - 1, nums, target);
+            if(lres == -1)
+                return mid;
+            else return lres;
+        }
+        int lres = divide(l, mid - 1, nums, target);
+        int rres = divide(mid + 1,r,nums,target);
+        if(lres == -1 && rres == -1)
+            return -1;
+        if(lres == -1 || rres == -1)
+            return Math.max(lres,rres);
+        return lres;
+
+    }
+
+    private int getMid(int l, int r, int[] nums, int target) {
+        int mid = l + ( r - l) / 2;
+        if(nums[mid] == target){
+            int temp = mid;
+            while (mid >= 0 && nums[mid] == target){
+                mid--;
+            }
+            if(nums[mid + 1] == target)
+                return mid + 1;
+            else return temp;
+        }else return mid;
     }
     private int binSearch(int l,int r,int[] nums,int target){
         if(l>r)
             return -1;
         while (l<=r){
-            int mid = l + (r - l) /2;
+            int mid = getMid(l,r,nums,target);
             if(nums[mid] == target)
                 return mid;
             else if(nums[mid] > target){
@@ -29,5 +59,13 @@ public class MIsChen_search {
             }
         }
         return -1;
+    }
+    public static void main(String[] args){
+        MIsChen_search q = new MIsChen_search();
+        System.out.println(
+                q.search(new int[]{
+                        2, 1, 2, 2, 2
+                },2)
+        );
     }
 }
