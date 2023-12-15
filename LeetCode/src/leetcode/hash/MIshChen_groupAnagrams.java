de93af69c101969b997397ac4b5645d32ccf1589
laiyifaa <512879089@qq.com>
2023-12-15 09:59:26 +0800

    leetcode 面试题 10.02. 变位词组 经典素数思想

--- LeetCode/src/leetcode/hash/MIshChen_groupAnagrams.java
+++ LeetCode/src/leetcode/hash/MIshChen_groupAnagrams.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class MIshChen_groupAnagrams {
 
+    //素数思想 用来配乘积
     int[]  words = new int[]{
             2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101
     };
