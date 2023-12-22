cfed42626054e6b6047d8ac55fc01be6925982e9
laiyifaa <512879089@qq.com>
2023-12-22 20:56:19 +0800

    leetcode面试题 16.10. 生存人数 学习了新算法 差分数组 还可用前缀和

--- LeetCode/src/leetcode/array/MIsChen_maxAliveYear.java
+++ LeetCode/src/leetcode/array/MIsChen_maxAliveYear.java
@@ -6,7 +6,7 @@ public class MIsChen_maxAliveYear {
     //差分数组
     //1900 -2000
     //初始化都是0，year数组就是1900-2000的存活人数的差分数组
-    int[] year = new int[102];
+    int[] year = new int[102];/*
     public int maxAliveYear(int[] birth, int[] death) {
         for(int i = 0;i < birth.length; ++i){
             ++year[birth[i] - 1900];
@@ -25,6 +25,29 @@ public class MIsChen_maxAliveYear {
             x = y;
         }
         return ansYear + 1900 ;
+    }*/
+    //可用前缀和
+    public int maxAliveYear(int[] birth, int[] death) {
+        for(int i = 0;i < birth.length; ++i){
+            ++year[birth[i] - 1900];
+            --year[death[i] - 1900 + 1];
+        }
+        //构造前缀和数组
+        int[] preSum = new int[102];
+        preSum[0] = year[0];
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = year[i] + preSum[i-1];
+        }
+
+        int maxLives = -1;
+        int minyear = 0;
+        for (int i = 0; i < preSum.length; i++) {
+            if (preSum[i] > maxLives) {
+                minyear = i + 1900;
+                maxLives = preSum[i];
+            }
+        }
+        return minyear;
     }
     public static void main(String[] args){
         MIsChen_maxAliveYear q = new MIsChen_maxAliveYear();
