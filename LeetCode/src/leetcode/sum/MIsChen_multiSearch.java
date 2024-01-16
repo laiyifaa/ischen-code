4206592d5555a93bc73d46473e30307c556bbc76
laiyifaa <512879089@qq.com>
2024-01-16 11:18:27 +0800

    leetcode 面试题 17.17. 多次搜索 超时边缘，tire + 暴力(循环次数过多了)

--- LeetCode/src/leetcode/sum/MIsChen_multiSearch.java
+++ LeetCode/src/leetcode/sum/MIsChen_multiSearch.java
@@ -2,13 +2,13 @@ package leetcode.sum;
 
 import java.util.*;
 
-public class MmultiSearch {
+public class MIsChen_multiSearch {
     class Node{
-        String value = "";
+        int id = -1;
         Node[] childrens = new Node[26];
     }
     Node root;
-    Map<String, ArrayList<Integer>> map;
+    List<Integer>[] temp;
     //面试题 17.17. 多次搜索
     public int[][] multiSearch(String big, String[] smalls) {
         if(null == smalls || smalls.length == 0)
@@ -17,25 +17,21 @@ public class MmultiSearch {
             return new int[smalls.length][0];
         init(smalls);
         int[][] ans ;
-        for(String s : smalls){
-           addMap(s,big);
+        for(int i = 0;i < smalls.length;++i){
+           addMap(smalls[i],big,i);
         }
         ans = new int[smalls.length][];
         for(int i = 0;i < smalls.length ;++i){
-            String s = smalls[i];
-            ArrayList<Integer> list = map.getOrDefault(s,new ArrayList<>());
+            List<Integer> list = temp[i];
             ans[i] = new int[list.size()];
-            int cnt = 0;
             for(int j = 0; j < list.size();++j){
                 ans[i][j] = list.get(j);
             }
         }
         return ans;
     }
-    private void addMap(String s, String big) {
+    private void addMap(String s, String big,int id) {
         char[] chars = big.toCharArray();
-        if(s.length() <= 0)
-            return;
         for(int i = 0;i < chars.length;++i){
             Node cur = root;
             for(int j = i;j <  chars.length && j < i + s.length();++j){
@@ -45,34 +41,35 @@ public class MmultiSearch {
                     break;
                 }else cur = cur.childrens[index];
             }
-            if(cur.value.equals(s)){
-                map.get(s).add(i);
+            if(cur.id == id && s.length() > 0){
+                temp[id].add(i);
             }
         }
     }
     private void init(String[] smalls) {
         root = new Node();
-        map = new HashMap<>();
-        for(String s : smalls){
+        temp = new List[smalls.length];
+        for(int i = 0; i < smalls.length;++i){
             Node cur = root;
-            if(s.length() <= 0)
-                continue;
-            map.put(s,new ArrayList<>());
+            String s = smalls[i];
+            temp[i] = new ArrayList<>();
             for(char c : s.toCharArray()){
                 int index = c - 'a';
                 if(cur.childrens[index] == null)
                     cur.childrens[index] = new Node();
                 cur = cur.childrens[ index];
             }
-            cur.value = s;
+            cur.id = i;
         }
     }
     public static void main(String[] args){
-        MmultiSearch q = new MmultiSearch();
+        MIsChen_multiSearch q = new MIsChen_multiSearch();
         int[][] abcs = q.multiSearch("abc",
                 new String[]{
-                      ""
+                        ""
                 });
-        System.out.println(1);
+        System.out.println(
+                abcs
+        );
     }
 }
