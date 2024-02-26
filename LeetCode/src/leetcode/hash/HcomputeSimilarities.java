96412123d53577383a5b6fc5308b2f5810ffb6f1
laiyifaa <512879089@qq.com>
2024-02-26 15:39:34 +0800

    倒排索引：不再俩俩比较2个数组，而是遍历数组 以map辅助

--- LeetCode/src/leetcode/hash/HcomputeSimilarities.java
+++ LeetCode/src/leetcode/hash/HcomputeSimilarities.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class HcomputeSimilarities {
 
     //面试题 17.26. 稀疏相似度
-    //倒排索引
+    //倒排索引 : 思维转变一下，不是把两个数组比较，看有没有重复的。而是每次的数字出现，都去检查一下是否出现过，在哪个数组里出现的
     //小数点保留4位处理
     public List<String> computeSimilarities(int[][] docs) {
         if(null == docs || docs[0].length == 0)
@@ -28,10 +28,12 @@ public class HcomputeSimilarities {
                 }
                 list.add(i);
             }
-            for(int k = 0; k < docs.length;++k){
-                if(help[i][k] > 0){
-                    ans.add(k + "," + i + ": " + String.format("%.4f",(double)help[i][k]/(docs[i].length + docs[k].length - help[i][k])));
-                }
+
+        }
+        for(int  i = 0;i < docs.length;++i){
+            for(int j = 0; j <docs.length;++j){
+                if(help[i][j] > 0)
+                    ans.add(j + "," + i + ": " + String.format("%.4f",(double)help[i][j]/(docs[i].length + docs[j].length - help[i][j])));
             }
         }
         return  ans;
