26b7d70779c21a773cd1565a13e688d88c8f3672
laiyifaa <512879089@qq.com>
2024-04-11 11:33:46 +0800

    leetcode 173. 二叉搜索树迭代器 模拟

---
+++ LeetCode/src/leetcode/binaryTree/MBSTIterator.java
@@ -0,0 +1,33 @@
+package leetcode.binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class MBSTIterator {
+    private int idx;
+    private List<Integer> arr;
+
+    public MBSTIterator(TreeNode root) {
+        idx = 0;
+        arr = new ArrayList<Integer>();
+        inorderTraversal(root, arr);
+    }
+
+    public int next() {
+        return arr.get(idx++);
+    }
+
+    public boolean hasNext() {
+        return idx < arr.size();
+    }
+
+    private void inorderTraversal(TreeNode root, List<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, arr);
+        arr.add(root.val);
+        inorderTraversal(root.right, arr);
+    }
+}
+
