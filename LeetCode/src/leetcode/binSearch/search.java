1f5d1fc47a44124a1453ef68a25ac00bf2b2c6ed
laiyifaa <512879089@qq.com>
2024-05-17 10:13:29 +0800

    leetcode 704. 二分查找 二分模板

---
+++ LeetCode/src/leetcode/binSearch/search.java
@@ -0,0 +1,23 @@
+package leetcode.binSearch;
+
+public class search {
+
+    //二分查找
+    public int search(int[] nums, int target) {
+        if(null == nums)
+            return -1;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r){
+            int mid = l + (r - l) / 2 ;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] > target){
+                r = mid - 1;
+            }else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
