1c1afde0acf151e0c625d4687ed810031ac95e2b
laiyifaa <512879089@qq.com>
2024-05-24 10:54:39 +0800

    leetcode 502. IPO  优先队列 贪心  限制了w 那么先按capital 排序 然后把当前w满足的profits 入堆 并选择第i个利润加入w  反复循环 直至i = k

---
+++ LeetCode/src/leetcode/heap/HfindMaximizedCapital.java
@@ -0,0 +1,34 @@
+package leetcode.heap;
+
+import java.util.Arrays;
+
+import java.util.PriorityQueue;
+
+public class HfindMaximizedCapital {
+
+
+    //502.IPO
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        if(k <= 0 || profits.length == 0 )
+            return w;
+        int[][] arr = new int[profits.length][2];
+        for(int i = 0;i < profits.length;++i){
+            arr[i][0] = capital[i];
+            arr[i][1] = profits[i];
+        }
+        Arrays.sort(arr,(a,b) -> a[0]- b[0]);
+        int index  = 0 ;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> y - x);
+        for(int i = 0;i < k ;++i){
+            while (index < profits.length && arr[index][0] <= w  ){
+                pq.add(arr[index][1]);
+                ++index;
+            }
+            if(!pq.isEmpty()){
+                w += pq.poll();
+            }else break;;
+        }
+        return w;
+    }
+
+}
