b5ebc2b21dfa385fae6e619d8ce68f99cd72891f
laiyifaa <512879089@qq.com>
2024-06-03 16:31:39 +0800

    leetcode 188.买卖股票的最佳时机 IV tips: 参考k = 1 和 k = 2,多次买不买取决前一次的卖不卖。所以开数组记录 然后跑for循环

--- LeetCode/src/leetcode/dp/mult_dp/HIsChen_maxProfit.java
+++ LeetCode/src/leetcode/dp/mult_dp/HIsChen_maxProfit.java
@@ -1,6 +1,6 @@
 package leetcode.dp.mult_dp;
 
-public class MIsChen_maxProfit {
+public class HIsChen_maxProfit {
     //714. 买卖股票的最佳时机含手续费
     //0 和 1 代表是否持股
     public int maxProfit(int[] prices, int fee) {
@@ -25,7 +25,7 @@ public class MIsChen_maxProfit {
         return 0;
     }*/
 
-    //123. 买卖股票的最佳时机 III
+/*    //123. 买卖股票的最佳时机 III
     public int maxProfit(int[] prices) {
         if(null == prices || prices.length <= 1)
             return 0;
@@ -40,7 +40,34 @@ public class MIsChen_maxProfit {
             profit2 = Math.max(profit2,cost2 + prices[i]);
         }
         return profit2;
+    }*/
+
+    //188.买卖股票的最佳时机 IV
+    public int maxProfit(int k, int[] prices) {
+        if(null == prices || prices.length <= 1)
+            return 0;
+        k = Math.min(k,prices.length );
+        int[] cost = new int[k];
+        int[] profit = new int[k];
+        for(int i = 0 ;i < k;++i)
+            cost[i] = - prices[0];
+        for(int i = 1;i < prices.length;++i){
+            if(k == 1){
+                cost[0] = Math.max(cost[0],-prices[i]);
+                profit[0] = Math.max(profit[0],prices[i] + cost[0]);
+                continue;
+            }
+            for(int j = 0;j < k - 1;++j){
+                cost[j] = Math.max(cost[j],-prices[i]);
+                profit[j] = Math.max(profit[j],prices[i] + cost[j]);
+                cost[j + 1] = Math.max(cost[j + 1],profit[j] - prices[i]);
+                profit[j + 1] = Math.max(profit[j + 1],cost[j + 1] + prices[i]);
+            }
+        }
+        return profit[k - 1];
     }
-
-
+    public static void main(String[] args){
+        HIsChen_maxProfit q = new HIsChen_maxProfit();
+        System.out.println(q.maxProfit(1,new int[]{1,2}));
+    }
 }
