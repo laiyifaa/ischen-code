97370913ca4e301104623f15b6b932dc08c0c839
laiyifaa <512879089@qq.com>
2024-08-13 10:49:31 +0800

    LEETCODE 模拟

---
+++ LeetCode/src/main/java/leetcode/array/MshuffleArray.java
@@ -0,0 +1,40 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MshuffleArray {
+
+
+    int[] nums;
+    int[] original;
+
+    public MshuffleArray(int[] nums) {
+        this.nums = nums;
+        this.original = new int[nums.length];
+        for(int i = 0;i < nums.length;++i){
+            original[i] = nums[i];
+        }
+    }
+
+    public int[] reset() {
+        for(int i = 0;i < nums.length;++i){
+            nums[i] = original[i];
+        }
+        return nums;
+    }
+
+    public int[] shuffle() {
+        Random r = new Random();
+        List<Integer> list = new ArrayList<>(nums.length);
+        for(int i = 0;i < nums.length;++i){
+            list.add(nums[i]);
+        }
+        for(int i = 0;i < nums.length;++i){
+            nums[i] = list.remove(r.nextInt(list.size()));
+        }
+        return nums;
+    }
+
+}
