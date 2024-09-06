2fda840b8cc1332a45d6c73d417c022e39e65570
laiyifaa <512879089@qq.com>
2024-09-06 21:50:59 +0800

    leetcode1094. 拼车 差分思想

--- LeetCode/src/main/java/leetcode/graph/McanFinish.java
+++ LeetCode/src/main/java/leetcode/graph/McanFinish.java
@@ -1,5 +1,8 @@
 package leetcode.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class McanFinish {
 
     //207. 课程表
@@ -36,6 +39,45 @@ public class McanFinish {
         }
         return cnt == numCourses?true:false;
     }
+
+    //邻接表
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    public boolean canFinish_(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v: edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+    }
+
     public static void main(String[] args){
         McanFinish q = new McanFinish();
         System.out.println(
---
+++ LeetCode/src/main/java/leetcode/sum/McarPooling.java
@@ -0,0 +1,34 @@
+package leetcode.sum;
+
+public class McarPooling {
+
+    //1094. 拼车
+    //差分
+    public boolean carPooling(int[][] trips, int capacity) {
+        if(capacity == 0 || null == trips || trips.length == 0)
+            return true;
+        int[]d = new int[1005];
+        int end = 0;
+        for(int i = 0;i < trips.length;++i){
+            int[] trip = trips[i];
+            d[trip[1]] += trip[0];
+            d[trip[2]] -= trip[0];
+            if(trip[2] > end){
+                end = trip[2];
+            }
+        }
+        int temp = 0;
+        for(int i = 0;i <= end;++i){
+            temp += d[i];
+            if(temp > capacity)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        McarPooling q = new McarPooling();
+        System.out.println(q.carPooling(new int[][]{
+                {2,1,5},{3,3,7}
+        },5));
+    }
+}
